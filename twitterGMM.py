f0e08a70251939a91816f72700d361d5aa8f7b69
dwicke <dwicke@arch.com>
2013-04-24 12:28:14 -0400

    removed since moved.

--- twitterGMM.py
+++
@@ -1,8 +0,0 @@
-from twitterdata import TwitterData
-from twitterdata import TwitterUser
-
-
-d = TwitterData()
-
-d.twitterDataSetup('training_set_users.txt', 'training_set_tweets.txt')
-d.printStats()
