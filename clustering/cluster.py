ebe03febf814c787d4f307758406e3ff8f5dc1f3
Bryan Absher <bryan.absher@gmail.com>
2013-05-12 13:06:33 -0400

    updated clustering to support hierarchies and multiple clusters

--- clustering/cluster.py
+++ clustering/cluster.py
@@ -2,6 +2,10 @@ import sys
 import numpy as np
 import random
 import math
+from Queue import Queue, Empty
+from threading import Thread
+
+NUM_THREADS = 2
 
 def manhton(a,b):
     ret = 0
@@ -18,59 +22,110 @@ def euclidian(a,b):
 def distance(a, b):
     return euclidian(a,b)
 
+class Worker(Thread):
+    def __init__(self, queue, assignment, centroids):
+        Thread.__init__(self)
+        self.queue = queue
+        self.centroids = centroids
+        self.assignment = assignment
+    
+    def run(self):
+        try:
+            while not self.queue.empty():
+                (fidx, featureSet) = self.queue.get(True, .001)
+                dis = None
+                cluster = 0
+                # print "F: " + str(featureSet)
+                for centriod in self.centroids:
+                    distc = distance(featureSet, centriod)
+                    # print "\tD:" + str(distc) + " c:" + str(centriod) 
+                    if dis == None or dis > distc:
+                        dis = distc
+                        self.assignment[fidx] = cluster
+                    cluster += 1
+        except Empty:
+            pass
+
 def assign(features, c):
     assignment = np.zeros(features.shape[0])
+    
+    queue = Queue()
     fidx = 0
     for featureSet in features:
-        dis = None
-        cluster = 0
-        print "F: " + str(featureSet)
-        for centriod in c:
-            distc = distance(featureSet, centriod)
-            print "\tD:" + str(distc) + " c:" + str(centriod) 
-            if dis == None or dis > distc:
-                dis = distc
-                assignment[fidx] = cluster
-            cluster += 1
-        fidx += 1
+        queue.put((fidx, featureSet))
+        fidx = fidx + 1
+    
+    workers = []
+    for x in xrange(NUM_THREADS):
+        workers.append(Worker(queue, assignment, c))
+        
+    for w in workers:
+        w.start()
+        
+    for w in workers:
+        w.join()
+    
     return assignment
 
-def update(features, assignment):
-    centroids = np.zeros((2,2))
-    count = np.array([0,0])
+def update(features, assignment, numclusters):
+    centroids = np.zeros((numclusters,2))
+    count = np.zeros(numclusters)
     idx = 0
+    
     for a in assignment:
         centroids[a,0] += features[idx,0]
         centroids[a,1] += features[idx,1]
         count[a] += 1
         idx += 1
     # compute centriods, if there are any empty assign them to random point in feature set 
+    # return [[ctr/c for ctr in centroid] if not c == 0 else features[random.randint(0,features.shape[0]-1)] for (centroid,c) in zip(centroids,count)]
     return [[ctr/c for ctr in centroid] if not c == 0 else features[random.randint(0,features.shape[0]-1)] for (centroid,c) in zip(centroids,count)]
 
-def main():
-    data = open(sys.argv[1], 'r')
-    f = np.genfromtxt(data, delimiter=" ")
-    print f
-    print f.shape
-    numclusters = 2
-    c1 = random.randint(0,f.shape[0]-1)
-    c2 = random.randint(0,f.shape[0]-1)
-    print "Intializing with " + str(f[c1]) + " and " + str(f[c2])
-    centriods = [f[c1], f[c2]]
+def cluster(data, numclusters):
+    centroids = np.zeros((numclusters, 2))
+    for x in xrange(0,numclusters):
+        centroids[x] = data[random.randint(0,data.shape[0]-1)]
+    print "Initializing with centroids:\n" + str(centroids)
     assignment = []
     converged = False
     interation = 0
     while not converged:
         print "Iteration " + str(interation)
-        newAssign = assign(f, centriods)
+        newAssign = assign(data, centroids)
         # Test to see if our assignments are the same as before
         if np.array_equal(newAssign, assignment):
             converged = True
-        centriods = update(f, newAssign)
-        print "New centroids " + str(centriods)
+        centroids = update(data, newAssign, numclusters)
+        print "New centroids " + str(centroids)
         assignment = newAssign
         interation += 1
-    print assignment
+    return (assignment, centroids)
+
+def makeTree(tree, data, numclusters, MaxDepth, name):
+    if MaxDepth == 0:
+        return
+    (assignment, centroids) = cluster(data, numclusters)
+    tree[name] = centroids
+    
+    for x in xrange(numclusters):
+        indx = 0
+        d = []
+        for a in assignment:
+            if a == x:
+                d.append(data[indx])
+            indx = indx + 1
+        makeTree(tree, np.array(d), numclusters, MaxDepth-1, name+str(x))
+
+def main():
+    data = open(sys.argv[1], 'r')
+    f = np.genfromtxt(data, delimiter=" ")
+    numclusters = int(sys.argv[2])
+    MaxDepth = int(sys.argv[3])
+    print f
+    print f.shape
+    tree = dict()
+    makeTree(tree, f, numclusters, MaxDepth, 'root')
+    print tree
 
 if __name__ == "__main__":
     main()
